3768cf3d6f74d0f805bfb458e34f72c0cf34837d
printf_cherif <64066992+Maximus203@users.noreply.github.com>
2022-03-12 20:57:43 +0000

    Lab 5

--- Lab5/my_node_app/main.js
+++ Lab5/my_node_app/main.js
@@ -1,1 +1,49 @@
-"use strict";
+//const utils = require("./utils");
+
+//"use strict";
+const port = 3001,
+    http = require("http"),
+    httpStatus = require("http-status-codes"),
+    router = require("./router"),
+    contentTypes = require("./contentTypes"),
+    utils = require("./utils");
+
+router.get("/", (req, res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, contentTypes.html);
+    utils.getFile("views/index.html", res);
+}
+);
+
+router.get("/courses.html", (req, res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, contentTypes.html);
+    utils.getFile("views/courses.html", res);
+}
+);
+router.get("/contact.html", (req, res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, contentTypes.html);
+    utils.getFile("views/contact.html", res);
+}
+);
+
+router.get("/glob.css", (req, res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, contentTypes.css);
+    utils.getFile("public/css/glob.css", res);
+}
+);
+router.get("/bootstrap.css", (req, res) => {
+    res.writeHead(httpStatus.StatusCodes.OK, contentTypes.css);
+    utils.getFile("public/css/bootstrap.css", res);
+}
+);
+http.createServer(router.handle).listen(port);
+console.log(`The server is listening on port number: ${port}`);
+
+/*http.createServer(
+    ("/", (req, res) => {
+        res.writeHead(httpStatus.StatusCodes.OK, contentTypes.html);
+        res.end("Server is running");
+    })
+
+).listen(port)*/
+
+console.log(`The server is listening on port number: ${port}`);
