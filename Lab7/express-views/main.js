549b832be80b99c2b1747643d6b1f9f097592d79
printf_cherif <64066992+Maximus203@users.noreply.github.com>
2022-03-21 07:20:22 +0000

    Lab7

--- Lab7/express-views/main.js
+++ Lab7/express-views/main.js
@@ -3,14 +3,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const publicPath = path.resolve(__dirname, 'public');
-app.use(express.static(publicPath));
+//const publicPath = path.resolve(__dirname, 'public');
+//app.use(express.static(publicPath));
 
-app.use((req, res) => {
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/*app.use((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<h3>Fichier non trouve !</h3>");
+});*/
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        message: "Hey everyone! This is my webpage.",
+    });
 });
 
-http.createServer(app).listen(3300);
+http.createServer(app).listen(3000);
 
 
